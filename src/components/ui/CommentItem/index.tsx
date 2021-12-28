98e2fd2f03a3f2ab9bb6e7fb813256a76cfe35e3
shimotsu4431 <shimotsu13@gmail.com>
2021-12-28 14:38:49 +0900

    edit comment

--- src/components/ui/CommentItem/index.tsx
+++ src/components/ui/CommentItem/index.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import { PublicPhoto } from '../../../types/publicPhoto'
 import { Profile, useUser } from '../../../hooks/useUser'
@@ -7,6 +7,7 @@ import { supabase } from '../../../utils/supabaseClient'
 import { toast } from 'react-toastify'
 import Router from 'next/router'
 import { Comment } from '../../../types/comment'
+import { SUPABASE_BUCKET_COMMENTS_PATH } from '../../../utils/const'
 
 type props = {
   user: Profile
@@ -15,9 +16,39 @@ type props = {
 }
 
 export const CommentItem: React.FC<props> = ({ user, comment, photoData }) => {
+  const [isEditing, setIsEditing] = useState(false)
+  const [editComment, setComment] = useState<string>('')
+
   const {
     profile
   } = useUser()
+
+  const handleUpdate = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!profile) {
+      toast.info('ログインしてください。')
+
+      return
+    }
+
+    try {
+      await supabase.from(SUPABASE_BUCKET_COMMENTS_PATH)
+      .update({ body: editComment })
+      .match({ id: comment.id })
+      .single();
+
+      toast.success("コメントを更新しました！")
+      Router.push({
+        pathname: `/${user.fullname}/photo/${photoData.id}`,
+      }, undefined, { scroll: false })
+    } catch (err) {
+      toast.error("エラーが発生しました。")
+    } finally {
+      setComment('')
+      setIsEditing(false)
+    }
+  }
 
   const handleDelete = async (commentId: number) => {
     if (!window.confirm("コメント削除しますか？")) return
@@ -46,10 +77,31 @@ export const CommentItem: React.FC<props> = ({ user, comment, photoData }) => {
         </div>
       )}
       <p className='font-bold'>{comment.users.nickname}</p>
-      <p className='text-base'>{comment.body}</p>
-      {profile?.id === comment.userId && (
+      {!isEditing && <p className='text-base'>{comment.body}</p>}
+      {isEditing && (
+        <div>
+          <form onSubmit={handleUpdate} className='flex flex-col'>
+            <textarea
+              required
+              value={editComment ?? ''}
+              onChange={(e) => setComment(e.target.value)}
+              placeholder=""
+              className='border-gray-300 border-2 rounded p-1 mr-2 w-96'
+            />
+            <div>
+              <button type="submit" className='border-gray-300 border-2 rounded p-1 w-12 mt-2 mr-2'>更新</button>
+              <button onClick={() => setIsEditing(false)} className='border-gray-300 border-2 rounded p-1 w-16 mt-2'>閉じる</button>
+            </div>
+          </form>
+        </div>
+      )}
+      {profile?.id === comment.userId && !isEditing &&(
         <div className='mt-4'>
-          <button className='mr-2 underline'>編集</button>
+          <button onClick={() => {
+              setIsEditing(!isEditing)
+              setComment(comment.body)
+            }
+          } className='mr-2 underline'>編集</button>
           <button onClick={() => handleDelete(comment.id)} className='underline'>削除</button>
         </div>
       )}
